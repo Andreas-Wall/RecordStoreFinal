d26881567fc7b4d43ea4b5cb7b900d006e39b896
Andreas-Wall <60330641+Andreas-Wall@users.noreply.github.com>
2021-06-10 21:22:58 -0500

    Update Class1.cs

--- FinalProductsDLL/FinalProductsDLL/Class1.cs
+++ FinalProductsDLL/FinalProductsDLL/Class1.cs
@@ -16,7 +16,7 @@ namespace FinalProductsDLL{
         //creates a general connection to the database.
         public void connect(){
             try{
-                dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+                dataConnect = new SqlConnection("");
                 dataConnect.Open();
                 dataCommand = new SqlCommand("select p.*, v.VariantPrice, v.VariantStock from Products p join variant v on p.ASIN = v.VariantID order by p.Band", dataConnect);
                 dataAdapter = new SqlDataAdapter();
@@ -32,7 +32,7 @@ namespace FinalProductsDLL{
         public void fillTable(DataTable table){ dataAdapter.Fill(table); }
         //safely removes the product from the database
         public void deleteData(string id){
-            dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+            dataConnect = new SqlConnection("");
             dataConnect.Open();
             dataCommand = new SqlCommand("delete from cart where itemID =  '" + id + "'", dataConnect);
             DataReader = dataCommand.ExecuteReader();
@@ -46,7 +46,7 @@ namespace FinalProductsDLL{
             dataConnect.Close();}
         //updates the products information with picture
         public void updateData(string id, string name, string band, string price, string release, string label, string runtime, byte[] picture, string vprice){
-            dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+            dataConnect = new SqlConnection("");
             dataConnect.Open();
             dataCommand = new SqlCommand("update Products set AlbumName = '" + name + "', Band = '" + band + "', Price = '" + price + "', ReleaseDate = '" + release + "', Label = '" + label + "', Runtime = '" + runtime + "', Picture = @picture where ASIN =  '" + id + "'", dataConnect);
             dataCommand.Parameters.AddWithValue("@picture", picture);
@@ -59,7 +59,7 @@ namespace FinalProductsDLL{
         //updates the product without picture
         public void updateDataWOPicture(string id, string name, string band, string price, string release, string label, string runtime, string vprice)
         {
-            dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+            dataConnect = new SqlConnection("");
             dataConnect.Open();
             dataCommand = new SqlCommand("update Products set AlbumName = '" + name + "', Band = '" + band + "', Price = '" + price + "', ReleaseDate = '" + release + "', Label = '" + label + "', Runtime = '" + runtime + "' where ASIN =  '" + id + "'", dataConnect);
             DataReader = dataCommand.ExecuteReader();
@@ -71,7 +71,7 @@ namespace FinalProductsDLL{
         }
         //updates the products price
         public void updateStock(string id, string Stock, string vStock){
-            dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+            dataConnect = new SqlConnection("");
             dataConnect.Open();
             dataCommand = new SqlCommand("update Products set Stock = '" + Stock + "' where ASIN =  ('" + id + "')", dataConnect);
             DataReader = dataCommand.ExecuteReader();
@@ -82,7 +82,7 @@ namespace FinalProductsDLL{
             dataConnect.Close();}
         //inserts a new product into the database
         public void insert(string id, string name, string band, string price, string release, string label, string runtime, string stock, byte[] picture, string vprice, string vstock){
-            dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+            dataConnect = new SqlConnection("");
             dataConnect.Open();
             dataCommand = new SqlCommand("insert into Products values ('" + id + "','" + name + "','" + band + "','" + price + "','" + release + "','" + label + "','" + runtime + "','" + stock + "', @picture)", dataConnect);
             dataCommand.Parameters.AddWithValue("@picture", picture);
@@ -94,14 +94,14 @@ namespace FinalProductsDLL{
             dataConnect.Close();}
         //adds an invoice of the most recent change of stock or added item
         public void addInvoice(string id, string name, string Stock, string price, string vStock, string vprice, string total){ 
-            dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+            dataConnect = new SqlConnection("");
             dataConnect.Open();
             dataCommand = new SqlCommand("insert into Invoice values ('" + id + "','" + name + "','" + Stock + "','" + price + "','" + vStock + "','" + vprice + "','" + total + "')", dataConnect);
             DataReader = dataCommand.ExecuteReader();
             DataReader.Close();
             dataConnect.Close();}
         public void deleteInvoice(){
-            dataConnect = new SqlConnection("Server = cstnt.tstc.edu; Database = wallM_Final; User Id = mawall; password = 1175037");
+            dataConnect = new SqlConnection("");
             dataConnect.Open();
             dataCommand = new SqlCommand("delete from Invoice", dataConnect);
             DataReader = dataCommand.ExecuteReader();
